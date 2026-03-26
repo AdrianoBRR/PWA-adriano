e19133b98b66cdf73d9eddf123166de72aaf2dbc
AdrianoBRR <adriano.souza.cruz@hotmail.com>
2026-03-26 14:48:40 -0300

    fix tema dark padrão

--- projeto-pwa/app.js
+++ projeto-pwa/app.js
@@ -1,52 +1,17 @@
-const lista = document.getElementById('lista');
-const loader = document.getElementById('loader');
-const toast = document.getElementById('toast');
+function toggleTheme() {
+  document.body.classList.toggle("light");
 
-function showLoader(show) {
-  loader.classList.toggle('hidden', !show);
-}
-
-function showToast(msg) {
-  toast.textContent = msg;
-  toast.classList.remove('hidden');
-  setTimeout(() => toast.classList.add('hidden'), 2000);
-}
-
-function updateStatus() {
-  const status = document.getElementById('status');
-  if (navigator.onLine) {
-    status.textContent = 'Online';
-    status.style.background = 'green';
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
   } else {
-    status.textContent = 'Offline';
-    status.style.background = 'red';
+    localStorage.setItem("theme", "dark");
   }
 }
 
-window.addEventListener('online', updateStatus);
-window.addEventListener('offline', updateStatus);
+(function () {
+  const theme = localStorage.getItem("theme");
 
-updateStatus();
-
-async function carregarDados() {
-  showLoader(true);
-  lista.innerHTML = '';
-
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
-    const data = await response.json();
-
-    data.forEach(item => {
-      const li = document.createElement('li');
-      li.textContent = item.title;
-      lista.appendChild(li);
-    });
-
-    showToast('Dados atualizados 🚀');
-
-  } catch (error) {
-    showToast('Sem internet 😢');
+  if (theme === "light") {
+    document.body.classList.add("light");
   }
-
-  showLoader(false);
-}
+})();
